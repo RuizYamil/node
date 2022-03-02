a54ff1733223e4816ff0adb76d278bd992d5c26e
Yamil Espejo <yamilespejoruiz@gmail.com>
2022-03-02 14:05:20 -0300

    Servidor con Express

---
+++ Desafio 3/server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+
+let file = require('./main')
+const data = new file.Container('./product.txt');
+
+const app = express()
+const PORT = 8081
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+ })
+server.on("error", error => console.log(`Error en servidor ${error}`))
+
+app.get('/', (request, response) => {
+    response.send('<h1 style="color:blue">Bienvenidos al servidor express</h1>')
+})
+
+app.get('/products', (request, response) => {
+    data.getAll().then(value => {
+        return response.send(value)
+    });
+})
+
+app.get('/productRandom', (request, response) => {
+    data.getAll().then(value => {
+        let findId
+        for(let x of value){
+            findId = value.indexOf(x);
+        }
+        findId++
+        
+        let rand = Math.floor(Math.random()*findId);
+        rand++
+        
+        data.getById(rand).then(value => {
+            return response.send(value)
+        });
+    });
+})
