e9a4165cc4c7f3f31b81355e673ac5168f856e91
Yamil Espejo <yamilespejoruiz@gmail.com>
2022-03-22 18:10:29 -0300

    Fix Api Rest

--- Desafio 4/server.js
+++ Desafio 4/server.js
@@ -1,9 +1,10 @@
-const express = require('express')
-
-let file = require('./main')
-const data = new file.Container('./product.txt');
+const express = require('express');
+const { Router } = express
 
 const app = express()
+const router = Router()
+
+app.use('/static', express.static(__dirname + '/public'));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
@@ -13,42 +14,93 @@ const server = app.listen(PORT, () => {
  })
 server.on("error", error => console.log(`Error en servidor ${error}`))
 
+const error = {error: 'producto no encontrado'}
+
+let array = [
+    {
+        "title": "Escuadra",
+        "price": 123.45,
+        "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
+        "id": 1
+    },
+    {
+        "title": "Calculadora",
+        "price": 234.56,
+        "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
+        "id": 2
+    },
+    {
+        "title": "Globo Terráqueo",
+        "price": 345.67,
+        "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
+        "id": 3
+    }
+];
+
 app.get('/', (request, response) => {
-    response.send('<h1 style="color:blue">Bienvenidos al servidor express</h1>')
+    response.sendFile(__dirname + '/public/index.html')
 })
 
-app.get('/api/products', (request, response) => {
-    data.getAll().then(value => {
-        return response.send(value)
-    });
+router.get('/', (request, response) => {
+    return response.send(array);
 })
 
-app.get('/api/products/:id', (request, response) => {
+router.get('/:id', (request, response) => {
     const id = request.params.id;
-    data.getById(parseInt(id)).then(value => {
-        return response.send(value)
-    });
+    const value = array.find(element => element.id == id);
+    if(value == undefined){
+        return response.send(error)
+    }
+    return response.send(value);
 });
 
-app.post('/api/products', (request, response) => {
-    const json = request.body; 
-    data.save(json).then(value => {
-        return response.send(value)
-    })
-})
-
-app.put('/api/products/:id', (request, response) => {
-    const id = request.params.id;
+router.post('/', (request, response) => {
     const json = request.body;
     
-    data.getById(parseInt(id)).then(value => {
-        value = json
-        return response.send(value)
-    });
+    for(let y of array){
+        if(json.id === undefined){
+            json.id = 1;
+        }
+        if(y.id === json.id){
+            json.id++;
+        }   
+    }
+    array.push(json)
+    return response.send(array)
 })
 
-app.delete('/api/products/:id', (request, response) => {
+router.put('/:id', (request, response) => {
     const id = request.params.id;
-    data.deleteById(parseInt(id))
-    return response.send("Se elimino correctamente")
+    const json = request.body;
+
+    const value = array.find(element => element.id == id);
+    if(value == undefined){
+        return response.send(error)
+    }
+    
+    for(let x of array){
+        if(id == x.id){
+            let findId = array.indexOf(x);
+            array[findId] = json;
+        }
+    }
+    return response.send(array)
 })
+
+router.delete('/:id', (request, response) => {
+    const id = request.params.id;
+    const value = array.find(element => element.id == id);
+    if(value == undefined){
+        return response.send(error)
+    }
+    
+    for(let x of array){
+        if(id == x.id){
+            let findId = array.indexOf(x);
+            array.splice(findId, 1);
+        }
+    }
+    return response.send(array)
+})
+
+app.use('/api/products', router)
